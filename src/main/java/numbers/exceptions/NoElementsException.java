e9db3f83312328743352c23c143e8f89f1d7e93b
Игорь <igor.ivanov.grodno@gmail.com>
2020-05-13 21:55:08 +0300

    refactor code

--- src/main/java/numbers/exceptions/NoElementsException.java
+++ src/main/java/numbers/exceptions/NoElementsException.java
@@ -1,5 +1,11 @@
 package numbers.exceptions;
 
+/**
+ * Thrown when Numbers haven't any entered numbers and application attempts to call methods of Numbers
+ * 
+ * @author Igor Ivanov
+ * @since 13.05.2020
+ */
 public class NoElementsException extends Throwable
 {
 }
